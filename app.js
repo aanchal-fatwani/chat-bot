38b4fdae085f4fc9e12123df845f1265a000cf9c
Aanchal <63228776+aanchal-fatwani@users.noreply.github.com>
2022-09-10 22:33:44 +0530

    feat(chat-bot): add application files

---
+++ app.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+var stylus = require('stylus');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const userRouter = require('./routes/userRoutes');
+const viewRouter = require('./routes/viewRoutes');
+const conversationRouter = require('./routes/conversationRoutes');
+const pdfRouter = require('./routes/pdfRoutes');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(cors());
+
+app.use(
+  stylus.middleware({
+    src: __dirname + '/views',
+    dest: __dirname + '/public'
+  })
+);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(helmet());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  next();
+});
+
+app.use('/', viewRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/conversations', conversationRouter);
+app.use('/pdf', pdfRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
